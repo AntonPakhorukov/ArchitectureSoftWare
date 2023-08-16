b977c5ba2922159bd98254565464be2d61f537b2
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2023-08-16 19:16:33 +0700

    add diagrams

--- HomeWork_01/src/ModelElements/PoligonalModel.java
+++ HomeWork_01/src/ModelElements/PoligonalModel.java
@@ -7,8 +7,14 @@ public class PoligonalModel {
     public List<Poligon> Poligons; // так как poligonS => List
     public List<Texture> Textures; // так как textureS => List
 
+    /**
+     * Чтобы показать отношения Texture и Poligon, создается конструктор
+     * @param textures
+     */
     public PoligonalModel(List<Texture> textures) {
-        this.Textures = textures; // не закрашенный ромбик = агрегация = принимаем в качестве аргумента в конструктор
-        Poligons = new ArrayList<>(); // закрашенный ромбик = композиция = создаем внутри конструктора
+        this.Textures = textures; // не закрашенный ромбик = агрегация
+        // => принимаем в качестве аргумента в конструктор, может существовать обособленно
+        Poligons = new ArrayList<>(); // закрашенный ромбик = композиция
+        // => создаем внутри конструктора, не может существовать обособленно
     }
 }
